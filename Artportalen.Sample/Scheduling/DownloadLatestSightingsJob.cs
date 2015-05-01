8376133f0a26d4fae41f4a5a4bf7ff54079ff8d1
Magnus Unger <magnus_unger@hotmail.com>
2015-05-01 22:07:57 +0200

    prevent scheduled jobs to run multiple times concurrently

--- Artportalen.Sample/Scheduling/DownloadLatestSightingsJob.cs
+++ Artportalen.Sample/Scheduling/DownloadLatestSightingsJob.cs
@@ -1,6 +1,7 @@
 ﻿namespace Artportalen.Sample.Scheduling
 {
     using System;
+    using System.Collections.Generic;
     using System.Diagnostics;
     using System.Net.Http;
     using System.Threading;
@@ -21,6 +22,16 @@
         public void Execute(IJobExecutionContext jobContext)
         {
             Console.WriteLine("DownloadLatestSightingsJob started: {0}", DateTimeOffset.Now);
+
+            IList<IJobExecutionContext> jobs = jobContext.Scheduler.GetCurrentlyExecutingJobs();
+            foreach (IJobExecutionContext job in jobs)
+            {
+                if (job.Trigger.Equals(jobContext.Trigger) && !job.JobInstance.Equals(this))
+                {
+                    Console.WriteLine("There's another instance running, so leaving: " + this);
+                    return;
+                }
+            }
 
             var schedEnabled = System.Configuration.ConfigurationManager.AppSettings["SchedulingEnabled"];
             if (schedEnabled != "true")
--- Artportalen.Sample/Scheduling/DownloadTodaysSightingsJob.cs
+++ Artportalen.Sample/Scheduling/DownloadTodaysSightingsJob.cs
@@ -1,18 +1,7 @@
 ﻿namespace Artportalen.Sample.Scheduling
 {
     using System;
-    using System.Diagnostics;
-    using System.Net.Http;
-    using System.Threading;
-
-    using AppHarbor;
-
-    using Artportalen.Helpers;
-    using Artportalen.Model;
-    using Artportalen.Response;
-    using Artportalen.Sample.Data.Services;
-
-    using NLog;
+    using System.Collections.Generic;
 
     using Quartz;
 
@@ -21,6 +10,16 @@
         public void Execute(IJobExecutionContext jobContext)
         {
             Console.WriteLine("DownloadTodaysSightingsJob started: {0}", DateTimeOffset.Now);
+
+            IList<IJobExecutionContext> jobs = jobContext.Scheduler.GetCurrentlyExecutingJobs();
+            foreach (IJobExecutionContext job in jobs)
+            {
+                if (job.Trigger.Equals(jobContext.Trigger) && !job.JobInstance.Equals(this))
+                {
+                    Console.WriteLine("There's another instance running, so leaving" + this);
+                    return;
+                }
+            }
 
             var schedEnabled = System.Configuration.ConfigurationManager.AppSettings["SchedulingEnabled"];
             if (schedEnabled != "true")
